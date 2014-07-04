99bebc55b5af782189244bdada461b0dd876dcea
pmeisen <philipp@meisen.net>
2014-07-04 13:52:08 +0200

    - JavaDoc added

--- src/net/meisen/dissertation/jdbc/protocol/WrappedException.java
+++ src/net/meisen/dissertation/jdbc/protocol/WrappedException.java
@@ -1,6 +1,21 @@
 package net.meisen.dissertation.jdbc.protocol;
 
+/**
+ * An exception send from the server, which is wrapped over the communication
+ * {@code Protocol}.
+ * 
+ * @author pmeisen
+ * 
+ */
 public class WrappedException extends RuntimeException {
+	private static final long serialVersionUID = -813220420443909821L;
+
+	/**
+	 * Constructor which specifies the exception to be wrapped.
+	 * 
+	 * @param msg
+	 *            the exception wrapped by {@code this}
+	 */
 	public WrappedException(final String msg) {
 		super(msg);
 	}
