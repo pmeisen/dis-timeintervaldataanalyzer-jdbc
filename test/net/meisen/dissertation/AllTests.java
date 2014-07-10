a43a232b8e9e5ba74952fc80c364bb60923d92c9
pmeisen <philipp@meisen.net>
2014-07-10 10:24:06 +0200

    - activated all tests again

--- test/net/meisen/dissertation/AllTests.java
+++ test/net/meisen/dissertation/AllTests.java
@@ -18,12 +18,10 @@ import org.junit.runners.Suite;
  * 
  */
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ TestQueryResponseHandler.class
-//	, TestProtocol.class,
-//		TestTidaDriver.class, TestTidaConnection.class,
-//		TestObjectArrayResultSet.class, TestTidaStatement.class,
-//		TestTidaResultSet.class, TestTidaDatabaseMetaData.class 
-		})
+@Suite.SuiteClasses({ TestQueryResponseHandler.class, TestProtocol.class,
+		TestTidaDriver.class, TestTidaConnection.class,
+		TestObjectArrayResultSet.class, TestTidaStatement.class,
+		TestTidaResultSet.class, TestTidaDatabaseMetaData.class })
 public class AllTests {
 	// nothing more to do here
 }
